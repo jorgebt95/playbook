1d11dbfe1dd421b19ddda1bad0d0bbdb2fd84712
jorgebt95 <47363708+jorgebt95@users.noreply.github.com>
2022-05-18 00:07:13 -0500

    semana 3 avance 05-08-22

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,6 +1,14 @@
-describe("Test Suite Dummy Description", ()=>{
-	test('Case 1 Dummy', () =>{
-		const resultOfSomething = 20 - 2
-		expect(resultOfSomething).toBe(18);
+const Spiderman = require('./../app/spiderman')
+
+
+describe("Test Suite for Spidey  Description", ()=>{
+	test('Create an spiderman object', () =>{
+		const andreGarfield = new Spiderman("Spiderman Sony", 31, "Andre Garfield", 2, "Sony")
+
+		expect(andreGarfield.name).toBe("Spiderman Sony");
+		expect(andreGarfield.age).toBe(31);
+		expect(andreGarfield.actor).toBe("Andre Garfield");
+		expect(andreGarfield.movies).toBe(2);
+		expect(andreGarfield.studio).toBe("Sony");
 	});
 })
